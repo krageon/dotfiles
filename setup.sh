e6239348e1e59e60b7b9e9e16253d3a4d81ff12e
Tim Bazuin <tbazuin@transip.nl>
2015-07-19 15:14:11 +0200

    fixes setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-[[ -h "~/.tmux.conf" ]] && rm ~/.tmux.conf
-[[ -f "~/.tmux.conf" ]] && mv ~/.tmux.conf ~/.tmux.conf.original
+[[ -h ~/.tmux.conf ]] && rm ~/.tmux.conf
+[[ -f ~/.tmux.conf ]] && mv ~/.tmux.conf ~/.tmux.conf.original
 
-[[ -h "~/.vimrc" ]] && rm ~/.vimrc
-[[ -f "~/.vimrc" ]] && mv ~/.vimrc ~/.vimrc.original
+[[ -h ~/.vimrc ]] && rm ~/.vimrc
+[[ -f ~/.vimrc ]] && mv ~/.vimrc ~/.vimrc.original
 
-[[ -h "~/.zshrc" ]] && rm ~/.zshrc
-[[ -f "~/.zshrc" ]] && mv ~/.zshrc ~/.zshrc.original
+[[ -h ~/.zshrc ]] && rm ~/.zshrc
+[[ -f ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.original
 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
